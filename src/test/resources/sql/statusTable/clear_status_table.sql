c58b65c9c99fb73b722b8ea506563d9cbc745fa0
rostyslavpaliuha <rostyslavpaliuha@gmail.com>
2017-10-01 03:46:35 +0300

    clear from trash edit readme.md

--- src/test/resources/sql/statusTable/clear_status_table.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE "task management wizard"."Status";
--- src/test/resources/sql/statusTable/init_status_table.sql
+++
@@ -1,4 +0,0 @@
-insert into "task management wizard"."Status" (name) values('Ready for work');
-insert into "task management wizard"."Status" (name) values('In progress');
-insert into "task management wizard"."Status" (name) values('Review');
-insert into "task management wizard"."Status" (name) values('Done');

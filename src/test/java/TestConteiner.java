30f2e4a718ceff456267a13f900a1cbe1cb206fc
Oleg Shevchuk <papalom31@gmail.com>
2017-10-02 23:45:04 +0300

    add abstract dao222

--- src/test/java/TestConteiner.java
+++ src/test/java/TestConteiner.java
@@ -1,15 +1,12 @@
 
 import dao.TestConfig;
 import com.softserve.academy.configuration.MainAppConfig;
-import com.softserve.academy.dao.implementation.AbstractDao;
 import javax.sql.DataSource;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 // @RunWith(SpringJUnit4ClassRunner.class)

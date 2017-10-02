c6a3236441828955c9c19d7feeeccd6e91791513
Oleg Shevchuk <papalom31@gmail.com>
2017-10-02 12:15:40 +0300

    delete draft

--- src/main/java/com/softserve/if078/tmwSpring/dao/CommentDao.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.if078.tmwSpring.dao;
-
-import java.sql.SQLException;
-
-import com.softserve.if078.tmwSpring.entities.Comment;
-
-public interface CommentDao extends DaoInterface<Comment> {
-
-}

87816619187e702a3d4364228cbd5ce84fce1230
andrew.lozinskij <andrew.lozinskij@gmail.com>
2017-10-02 14:04:59 +0300

    change script

--- src/test/resources/h2_script.sql
+++ src/test/resources/h2_script.sql
@@ -1,113 +1,158 @@
-CREATE SCHEMA IF NOT EXISTS "tmw" ;
-USE "tmw" ;
+CREATE SCHEMA IF NOT EXISTS tmw;
+USE tmw;
 
 -- -----------------------------------------------------
--- Table `task management wizard`.`Users`
+-- Table tmw.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tmw"."User" (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tmw.user (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `pass` CHAR(32) NULL,
   `email` VARCHAR(254) NULL,
-  PRIMARY KEY (`user_id`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+
 -- -----------------------------------------------------
--- Table `task management wizard`.`Status`
+-- Table tmw.`status`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tmw"."Status" (
-  `status_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tmw.status(
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`status_id`))     
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+
 -- -----------------------------------------------------
--- Table `task management wizard`.`Priorities`
+-- Table tmw.`priority`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tmw"."Priority" (
-  `priority_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tmw.priority (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`priority_id`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+
 -- -----------------------------------------------------
--- Table `task management wizard`.`Tasks`
+-- Table tmw.`task`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tmw"."Task" (
-  `task_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tmw.task (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `created_date` TIMESTAMP NULL,
   `start_date` TIMESTAMP NULL,
   `end_date` TIMESTAMP NULL,
-  `estimate_time` TIMESTAMP NULL,
+  `estimate_time` TIME NULL,
   `assign_to` INT NULL,
   `status_id` INT NULL,
   `priority_id` INT NULL,
-  PRIMARY KEY (`task_id`),
+  `parent_id` INT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_Tasks_Status1`
     FOREIGN KEY (`status_id`)
-    REFERENCES "tmw"."Status" (`status_id`),  
+    REFERENCES tmw.status(`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_Tasks_Priorities1`
     FOREIGN KEY (`priority_id`)
-    REFERENCES "tmw"."Priority" (`priority_id`))   
+    REFERENCES tmw.priority (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
+
+
 -- -----------------------------------------------------
--- Table `task management wizard`.`Comment`
+-- Table tmw.`comment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tmw"."Comment" (
-  `comment_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tmw.comment (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `comment` TEXT NOT NULL,
-  `created_date` TIMESTAMP NULL,
+  `created_date` DATETIME NULL,
   `task_id` INT NOT NULL,
   `user_id` INT NOT NULL,
-  PRIMARY KEY (`comment_id`))
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_Comments_Tasks1`
+    FOREIGN KEY (`task_id`)
+    REFERENCES tmw.task (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_Comments_Users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES tmw.user (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `task management wizard`.`Tags`
+-- Table tmw.`tag`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tmw"."Tag" (
-  `tag_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tmw.tag (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `user_id` INT NOT NULL,
-  PRIMARY KEY (`tag_id`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_Tags_Users1`
     FOREIGN KEY (`user_id`)
-    REFERENCES "tmw"."User" (`user_id`))
+    REFERENCES tmw.user (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `task management wizard`.`tags_tasks`
+-- Table tmw.`tags_tasks`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tmw"."tags_tasks" (
-  `tag_task_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tmw.tags_tasks (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `tag_id` INT NOT NULL,
   `task_id` INT NOT NULL,
-  PRIMARY KEY (`tag_task_id`, `tag_id`, `task_id`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_tags_tasks_Tags1`
     FOREIGN KEY (`tag_id`)
-    REFERENCES "tmw"."Tag" (`tag_id`), 
+    REFERENCES tmw.tag (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_tags_tasks_Tasks1`
     FOREIGN KEY (`task_id`)
-    REFERENCES "tmw"."Task" (`task_id`))
-ENGINE = InnoDB;
--- -----------------------------------------------------
--- Table `task management wizard`.`Roles`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tmw"."Role" (
-  `role_id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`role_id`))
+    REFERENCES tmw.task (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `task management wizard`.`users_tasks`
+-- Table tmw.`role`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "tmw"."users_tasks" (
-  `users_tasks_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tmw.role(
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table tmw.`users_tasks`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS tmw.users_tasks (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `task_id` INT NOT NULL,
   `role_id` INT NOT NULL,
-  PRIMARY KEY (`users_tasks_id`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_users_tasks_Users1`
     FOREIGN KEY (`user_id`)
-    REFERENCES "tmw"."User" (`user_id`),    
+    REFERENCES tmw.user(`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_users_tasks_Tasks1`
     FOREIGN KEY (`task_id`)
-    REFERENCES "tmw"."Task" (`task_id`),
+    REFERENCES tmw.task(`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_users_tasks_Roles1`
     FOREIGN KEY (`role_id`)
-    REFERENCES "tmw"."Role" (`role_id`))
+    REFERENCES tmw.role (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;

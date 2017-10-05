308ede48d048aaeb27704958c53e64a7a06ea2b2
Oleg Shevchuk <papalom31@gmail.com>
2017-10-05 23:16:07 +0300

    llitle changes # xz

--- src/main/java/com/softserve/academy/controller/TagController.java
+++ src/main/java/com/softserve/academy/controller/TagController.java
@@ -1,9 +1,54 @@
 
 package com.softserve.academy.controller;
 
+import com.softserve.academy.entity.Tag;
+import com.softserve.academy.service.implementation.TagService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("user/{uId}/tags")
 public class TagController {
 
+  @Autowired
+  private TagService service;
+
+  @GetMapping(value = "/all")
+  public List<Tag> getTagsByUserId(@PathVariable Integer uId) {
+    return service.getAllByUserId(uId);
+  }
+
+  @PostMapping(value = "/")
+  public Tag createTag(@RequestBody Tag tag) {
+    return service.create(tag);
+  }
+
+  @GetMapping("/{tId}")
+  public Tag findTag(@PathVariable Integer tId) {
+    return service.findOne(tId);
+  }
+
+  @PutMapping("/")
+  public boolean updateTag(@RequestBody Tag tag) {
+    return service.update(tag);
+  }
+
+  @DeleteMapping("/{tId}")
+  public boolean deleteTag(@PathVariable Integer tId) {
+    return service.delete(tId);
+  }
+
+  @DeleteMapping("/")
+  public boolean deleteAllUserTags(@PathVariable Integer tId) {
+    return service.deleleAllByUserId(tId);
+  }
 }
--- src/main/java/com/softserve/academy/controller/TaskController.java
+++ src/main/java/com/softserve/academy/controller/TaskController.java
@@ -1,11 +1,9 @@
 package com.softserve.academy.controller;
 
 import com.softserve.academy.entity.Task;
-import com.softserve.academy.service.interfaces.TaskServiceInterface;
-
+import com.softserve.academy.service.implementation.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.sql.SQLException;
 import java.util.List;
 
@@ -13,85 +11,67 @@ import java.util.List;
 @RequestMapping("tasks")
 public class TaskController {
 
-    @Autowired
-    TaskServiceInterface taskDao;
-    /*@Autowired
-    TaskDao taskDao;*/
+  @Autowired
+  TaskService taskService;
+  /*
+   * @Autowired TaskDao taskService;
+   */
 
 
-    @PostMapping("/")
-    void createTask(@RequestBody Task task) throws SQLException {
-        taskDao.create(task);
-    }
+  @PostMapping("/")
+  void createTask(@RequestBody Task task) throws SQLException {
+    taskService.create(task);
+  }
 
-    @PutMapping("/")
-    void updateTask(@RequestBody Task task) throws SQLException {
-        taskDao.update(task);
-    }
+  @PutMapping("/")
+  void updateTask(@RequestBody Task task) throws SQLException {
+    taskService.update(task);
+  }
 
-    @DeleteMapping("/{id}")
-    void deleteTask(@PathVariable int id) throws SQLException {
-        taskDao.delete(id);
-    }
+  @DeleteMapping("/{id}")
+  void deleteTask(@PathVariable int id) throws SQLException {
+    taskService.delete(id);
+  }
 
-    @GetMapping("/")
-    List<Task> getAllTasks() throws SQLException {
-        return taskDao.getAll();
-    }
+  @GetMapping("/")
+  List<Task> getAllTasks() throws SQLException {
+    return taskService.getAll();
+  }
 
-    @GetMapping("/{id}")
-    Task getTask(@PathVariable int id) throws SQLException {
-        return taskDao.get(id);
-    }
+  @GetMapping("/{id}")
+  Task getTask(@PathVariable int id) throws SQLException {
+    return taskService.findOne(id);
+  }
 
-    /*@GetMapping("/tag/{id}")
-    List<Task> tasksByTag(@PathVariable int id) {
-        return taskDao.getTasksByTag(id);
-    }
-
-    @GetMapping("/priority/{id}")
-    List<Task> tasksByPriority(@PathVariable int id) {
-        return taskDao.getTaskByPriority(id);
-    }
-
-    @GetMapping("/status/{id}")
-    List<Task> tasksByStatus(@PathVariable int id) {
-        return taskDao.getTaskByStatus(id);
-    }
-
-    @GetMapping("/today")
-    List<Task> tasksForToday() {
-        return taskDao.getTasksForToday();
-    }
-
-    @GetMapping("/assign_to/{id}")
-    List<Task> assignTo(@PathVariable int id) {
-        return taskDao.getTasksAssignToUser(id);
-    }
-
-    @GetMapping("/created_by/{id}")
-    List<Task> createdBy(@PathVariable int id) {
-        return taskDao.getTasksCreatedByUser(id);
-    }
-
-    @GetMapping("/tree_of/{id}")
-    List<Task> treeOfTasks(@PathVariable int id) {
-        return taskDao.treeOfTasks(id);
-    }
-
-    @GetMapping("/author_of_task/{id}")
-    User authorOfTssk(@PathVariable int id) {
-        return taskDao.getAuthorOfTask(id);
-    }
-
-    @GetMapping("/tags_of/{id}")
-    List<Tag> tagsOfTask(@PathVariable int id) {
-        return taskDao.getTagsOfTask(id);
-    }
-
-    @GetMapping("/comments_of/{id}")
-    List<Comment> commentsOfTask(@PathVariable int id) {
-        return taskDao.getCommentsOfTask(id);
-    }*/
+  /*
+   * @GetMapping("/tag/{id}") List<Task> tasksByTag(@PathVariable int id) { return
+   * taskService.getTasksByTag(id); }
+   * 
+   * @GetMapping("/priority/{id}") List<Task> tasksByPriority(@PathVariable int id) { return
+   * taskService.getTaskByPriority(id); }
+   * 
+   * @GetMapping("/status/{id}") List<Task> tasksByStatus(@PathVariable int id) { return
+   * taskService.getTaskByStatus(id); }
+   * 
+   * @GetMapping("/today") List<Task> tasksForToday() { return taskService.getTasksForToday(); }
+   * 
+   * @GetMapping("/assign_to/{id}") List<Task> assignTo(@PathVariable int id) { return
+   * taskService.getTasksAssignToUser(id); }
+   * 
+   * @GetMapping("/created_by/{id}") List<Task> createdBy(@PathVariable int id) { return
+   * taskService.getTasksCreatedByUser(id); }
+   * 
+   * @GetMapping("/tree_of/{id}") List<Task> treeOfTasks(@PathVariable int id) { return
+   * taskService.treeOfTasks(id); }
+   * 
+   * @GetMapping("/author_of_task/{id}") User authorOfTssk(@PathVariable int id) { return
+   * taskService.getAuthorOfTask(id); }
+   * 
+   * @GetMapping("/tags_of/{id}") List<Tag> tagsOfTask(@PathVariable int id) { return
+   * taskService.getTagsOfTask(id); }
+   * 
+   * @GetMapping("/comments_of/{id}") List<Comment> commentsOfTask(@PathVariable int id) { return
+   * taskService.getCommentsOfTask(id); }
+   */
 
 }

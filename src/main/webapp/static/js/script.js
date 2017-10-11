67fe718141463987c8cd0d9292a056ba22818ca3
rostyslavpaliuha <rostyslavpaliuha@gmail.com>
2017-10-11 18:06:14 +0300

    change in UI

---
+++ src/main/webapp/static/js/script.js
@@ -0,0 +1,45 @@
+    $(document).ready(function () {
+        $("#welcome-container").show();
+        $("#tasks-btn").click(function () {
+            clearContent();
+            $(".task").remove();
+            getTasks();
+            $("#task-container").show();
+        })
+        $("#for-today-btn").click(function () {
+            clearContent();
+            $("#today-container").show();
+        })
+        $("#sprint-btn").click(function () {
+            clearContent();
+            $("#sprint-container").show();
+        })
+    });
+
+    function getTasks() {
+        $.ajax({
+            url: "tasks/",
+            type: "GET",
+            success: function (data) {
+                var list = JSON.parse(data);
+                renderTasks(list);
+            }
+        });
+    }
+
+    function clearContent() {
+        $(".content").hide();
+    }
+
+    function renderTasks(list) {
+
+        for (var task = 0; task < list.length; task++) {
+            var id = "task" + list[task].id;
+            var searchId = "#" + id;
+            var name = list[task].name;
+            var priority = list[task].priority;
+            var createdDate = list[task].created_date;
+            $("#task-container").append("<div id='" + id + "' class='task'></div>");
+            $(searchId).html("<p class='name'>" + name + "</p><p class='priority'>" + priority + "</p><p class='createdday'>" + createdDate + "</p><div class='task-nav'><button class='tag-btn'>tag</button><button class='priority-btn'>priority</button> <button class='status-btn'>status</button></div>");
+        }
+    }

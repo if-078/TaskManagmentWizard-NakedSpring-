9e3ab2db29d09089d6fdc9d741494fafdaa08d0f
Aurtom Mikhalov <muhadroid100@gmail.com>
2017-10-25 17:51:04 +0300

    half of road

--- src/main/java/com/softserve/academy/dao/implementation/JooqSQLBuilder.java
+++ src/main/java/com/softserve/academy/dao/implementation/JooqSQLBuilder.java
@@ -1,22 +1,31 @@
 package com.softserve.academy.dao.implementation;
 
 import org.jooq.*;
+import org.jooq.conf.ParamType;
+import org.jooq.conf.Settings;
 
 import static org.jooq.impl.DSL.*;
 
 public class JooqSQLBuilder {
-    DSLContext creator = using(SQLDialect.MYSQL);
+    Settings s = new Settings().withParamType(ParamType.NAMED);
+    DSLContext creator = using(SQLDialect.MYSQL, s);
+    Field id = field("task.id");
+    Field name = field("task.name");
+    Field crDate = field("task.created_date");
+    Field startDate = field("task.start_date");
+    Field endDate = field("task.end_date");
+    Field estimateTime = field("task.estimate_time");
+    Field statusId = field("task.status_id");
 
-    public Condition initialCondition (int id){
-        Condition condition = (field("USER.ID").eq(id));
-        return condition;
+    public SelectJoinStep initialCondition (int id){
+        SelectJoinStep s = select(name, crDate, startDate, endDate, estimateTime).from("task");
+        return s;
     }
 
-    public Condition addStatusCondition(String[] statuses, Condition condition){
-        for (String s : statuses) {
-            condition = condition.and(field("STATUS.NAME").eq(s));
-        }
-        return condition;
+    public SelectOnConditionStep addTagsJoin(SelectJoinStep selectSelectStep){
+        SelectOnConditionStep step = selectSelectStep.join("tags_tasks")
+                .on(field(id).eq(field("tags_tasks.task_id")));
+        return step;
     }
 
     public Condition addPriorityCondition(String[] priorities, Condition condition){
@@ -27,14 +36,15 @@ public class JooqSQLBuilder {
     }
 
     public static void main(String[] args) {
-        String[] statuses = new String[]{"st1", "st2", "st3"};
-        String[] priorities = new String[]{"P1", "p2"};
         JooqSQLBuilder builder = new JooqSQLBuilder();
-        Condition condition = builder.initialCondition(2);
-        condition = builder.addPriorityCondition(priorities, condition);
-        condition = builder.addStatusCondition(statuses, condition);
-        String res =  builder.creator.select(field("TASK.ID")).from("Task").join("Users").on(field("USER.ID").eq("USER_ID")).where(condition).getSQL();
+        System.out.println(builder.initialCondition(2).getSQL());
 
-        System.out.println(res);
     }
 }
+/* select task_id from tags_tasks where tag_id in(1,3)
+group by task_id having count(distinct tag_id) = 2;
+select task.id, task.name, status.name, priority_id from task
+join tags_tasks on task.id = tags_tasks.task_id
+join tag on tags_tasks.tag_id = tag.id
+join status on status.id = task.status_id
+where tag.name in ('tag1' , 'tag2')group by task_id having count(distinct tag_id) =2 and status.name = 'to do';*/

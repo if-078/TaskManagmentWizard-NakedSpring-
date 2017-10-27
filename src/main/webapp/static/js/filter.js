eecc79a1bee448776d8bea181938fef0212618b7
Oleg Shevchuk <papalom31@gmail.com>
2017-10-27 22:26:27 +0300

    add logic to clean button

--- src/main/webapp/static/js/filter.js
+++ src/main/webapp/static/js/filter.js
@@ -1,4 +1,11 @@
 
+function myformatter(date){
+    var y = date.getFullYear();
+    var m = date.getMonth()+1;
+    var d = date.getDate();
+    return y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d);
+}
+
 function Filter(startDate, endDate) {
     this.startDate=startDate;
     this.endDate=endDate;
@@ -21,14 +28,21 @@ $("#filterButton").on("click", function () {
     sendFilterOptions(setFilerValues());
 });
 
-function setFilerValues() {
-    var myFilter = new Filter($("#start").val(), $("#end").val());
-    myFilter.setPriority($("#priorityBox").val().split(","));
-    myFilter.setStatus($("#statusBox").val().split(","));
-    myFilter.setTag($("#tagBox").val().split(","));
-    alert('action');
-    console.log(JSON.stringify(myFilter));
-    return myFilter
+$("#cleanButton").on("click", function () {
+    clearFilterBlocks();
+});
+
+
+function resetForm($form) {
+
+    $form.find('input:text, input:password, input:span, select, textarea, a').val('');
+    $form.find('input:radio, input:checkbox')
+        .removeAttr('checked').removeAttr('selected');
+}
+
+function clearFilterBlocks(){
+    $("#statusBox, #priorityBox, #tagBox").combobox('clear');
+    $("#start, #end").datebox('clear');
 }
 
 function sendFilterOptions(filter){
@@ -41,3 +55,14 @@ $.post('users/1/filter', JSON.stringify(filter))
     })
 ;
 }
+
+function setFilerValues() {
+    var myFilter = new Filter($("#start").val(), $("#end").val());
+    myFilter.setPriority($("#priorityBox").val().split(","));
+    myFilter.setStatus($("#statusBox").val().split(","));
+    myFilter.setTag($("#tagBox").val().split(","));
+    alert('action');
+    console.log(JSON.stringify(myFilter));
+    return myFilter
+}
+

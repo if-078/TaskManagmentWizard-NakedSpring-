d8d4fa5cb1a0affbe96417734493d28dc0b0b7f2
Oleg Shevchuk <papalom31@gmail.com>
2017-10-31 12:01:43 +0200

    clean old proj

--- src/main/java/com/softserve/academy/dao/interfaces/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.academy.dao.interfaces;
-
-
-import com.softserve.academy.dao.interfaces.EntityDao;
-import com.softserve.academy.entity.User;
-
-public interface UserDao extends EntityDao<User> {
-	User findByEmail(String email);
-}

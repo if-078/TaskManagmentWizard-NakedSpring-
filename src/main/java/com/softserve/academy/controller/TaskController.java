48a9e556653c71a3b41b368b593e830dcd5ac8c9
Dimitrij512 <dmytro.andrusiv@gmail.com>
2017-10-31 21:11:57 +0200

    test commit

--- src/main/java/com/softserve/academy/controller/TaskController.java
+++ src/main/java/com/softserve/academy/controller/TaskController.java
@@ -39,7 +39,7 @@ public class TaskController {
   }
 
 
-  //================================================================
+  //============================================================
 
   @GetMapping
   List<TaskTableDTO> getTasksByFilter(

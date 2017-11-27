ccaeee430980cf13d1fdd7958ebff819908e8e61
RomanMaliarchuk <roman.malarchuk@gmail.com>
2017-11-28 01:25:25 +0200

    corected ajax and controller for get team list

--- src/main/java/com/softserve/academy/tmw/controller/UserController.java
+++ src/main/java/com/softserve/academy/tmw/controller/UserController.java
@@ -6,15 +6,7 @@ import java.sql.SQLException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/users")
@@ -23,9 +15,13 @@ public class UserController {
   @Autowired
   UserServiceInterface userService;
 
-  @GetMapping("/all")
+  @GetMapping("/team/{taskId}")
   @ResponseStatus(HttpStatus.OK)
-  List<User> getAllUsers() throws SQLException {
+  List<User> getAllUsers(@PathVariable Integer taskId, @RequestParam(name = "userId", required = false) int userId) throws SQLException {
+    System.out.println("====================");
+    System.out.println("taskId = " + taskId);
+    System.out.println("userId = " + userId);
+    System.out.println("====================");
     return userService.getAll();
   }
 

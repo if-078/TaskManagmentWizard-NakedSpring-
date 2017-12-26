4d2135ee1f44b1c9ab6ad04f678b05413c6924ec
Oleh Shevchuk <oshevchuk@softserveinc.com>
2017-12-26 11:33:25 +0200

    change one test

--- src/test/java/it/com/softserve/academy/tmw/controller/StatusControllerTest.java
+++ src/test/java/it/com/softserve/academy/tmw/controller/StatusControllerTest.java
@@ -1,4 +1,14 @@
 package it.com.softserve.academy.tmw.controller;
 
+import it.com.softserve.academy.tmw.configuration.TestConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestConfig.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class StatusControllerTest {
+
 }
